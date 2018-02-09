a30d8c843740b51e0d2bef5d29b318bc42dff4d6
Bundit Sankhumpha <sankhumpha84@gmail.com>
2018-02-10 05:45:27 +0700

    title v35

--- application/views/header.php
+++ application/views/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset=utf-8>
-    <title>TESCO LOTUS LEARNING CENTER</title>
+    <title>v35 - TESCO LOTUS LEARNING CENTER</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     
     <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/fonts/font.css">
